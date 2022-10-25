6b025cc0e1ed9402e196a53c16bda4eb4406e6a4
nowgnas <marshmello.sw@gmail.com>
2022-10-26 00:59:18 +0900

    fix login

--- src/main/java/com/ssafy/home/security/mapper/SecurityMapper.java
+++ src/main/java/com/ssafy/home/security/mapper/SecurityMapper.java
@@ -1,5 +1,6 @@
 package com.ssafy.home.security.mapper;
 
+import com.ssafy.home.member.dto.Member;
 import com.ssafy.home.security.dto.SecVO;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -7,4 +8,5 @@ import org.apache.ibatis.annotations.Mapper;
 public interface SecurityMapper {
     void insertSecurity(SecVO secVO);
 
+    SecVO selectByUserId(String userId);
 }
