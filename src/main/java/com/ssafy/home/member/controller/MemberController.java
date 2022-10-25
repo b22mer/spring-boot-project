ec7ebe9b206cf684005a9bcc58423181345f53b6
nowgnas <marshmello.sw@gmail.com>
2022-10-25 18:55:59 +0900

    amend member controller

--- src/main/java/com/ssafy/home/member/controller/MemberController.java
+++ src/main/java/com/ssafy/home/member/controller/MemberController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/user")
@@ -37,5 +36,4 @@ public class MemberController {
         memberService.register(member);
         return "register ok";
     }
-
 }
